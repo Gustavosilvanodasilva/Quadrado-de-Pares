b1d8752e56b33c03bad40c91e676e383dfffd388
Gustavo <Gustavo@DESKTOP-36SHUBO>
2023-01-10 17:50:56 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿double valor1;
+
+Console.WriteLine("Informe o valor 1");
+valor1 = Convert.ToDouble(Console.ReadLine());
+
+for(int i =2; i <= valor1; i++)
+{
+
+    if (i % 2 == 0)
+    {
+        Console.WriteLine("O quadrado é : " + i*i);
+    }
+}
